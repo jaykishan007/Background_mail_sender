4300ae4ef0111477c21fd3ba65281b6f9deccfa0
jaykishan1 <jayakishan100@gmail.com>
2017-04-04 18:12:15 +0530

    Email Dispatcher

---
+++ app/src/main/java/com/example/jaykishan/mroads/GMail.java
@@ -0,0 +1,101 @@
+package com.example.jaykishan.mroads;
+
+
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.SendFailedException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class GMail {
+
+    final String emailPort = "587";// gmail's smtp port
+    final String smtpAuth = "true";
+    final String starttls = "true";
+    final String emailHost = "smtp.gmail.com";
+
+    private String fromEmail="jayakishan100@gmail.com";
+    private String fromPassword="52701021";
+    private String toEmail;
+    private String emailSubject="Mroads";
+    private String emailBody="Message Drafted";
+
+    Properties emailProperties;
+    Session mailSession;
+    MimeMessage emailMessage;
+
+    public GMail() {
+
+    }
+
+    public GMail(
+                 String toEmail) {
+        this.toEmail = toEmail;
+
+        emailProperties = System.getProperties();
+        emailProperties.put("mail.smtp.port", emailPort);
+        emailProperties.put("mail.smtp.auth", smtpAuth);
+        emailProperties.put("mail.smtp.starttls.enable", starttls);
+        Log.i("GMail", "Mail server properties set.");
+    }
+
+    public MimeMessage createEmailMessage() throws AddressException,
+            MessagingException, UnsupportedEncodingException {
+
+        mailSession = Session.getDefaultInstance(emailProperties, null);
+        emailMessage = new MimeMessage(mailSession);
+
+
+        emailMessage.setFrom(new InternetAddress(fromEmail, fromEmail));
+
+            Log.i("GMail","toEmail: "+toEmail);
+            emailMessage.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toEmail.toString()));
+
+        emailMessage.setSubject(emailSubject);
+
+        Multipart multipart = new MimeMultipart("related");
+
+        BodyPart htmlPart = new MimeBodyPart();
+        htmlPart.setContent(emailBody, "text/html");
+        multipart.addBodyPart(htmlPart);
+        emailMessage.setContent(multipart);
+        // for a html email
+
+
+        Log.i("GMail", "Email Message created.");
+        return emailMessage;
+    }
+
+    public int sendEmail() throws AddressException, MessagingException {
+
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(emailHost, fromEmail, fromPassword);
+        Log.i("GMail","allrecipients: "+emailMessage.getAllRecipients().toString());
+        try {
+            transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+        }
+        catch (SendFailedException ex)
+        {
+            return 0;
+        }
+
+            transport.close();
+
+        Log.i("GMail", "Email sent successfully.");
+        return 1;
+    }
+
+}
---
+++ app/src/main/java/com/example/jaykishan/mroads/MainActivity.java
@@ -0,0 +1,189 @@
+package com.example.jaykishan.mroads;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    final Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+
+//                final Dialog dialog = new Dialog(context,R.style.Dialog);
+//                dialog.setContentView(R.layout.custom_dialog);
+//
+//                dialog.setTitle("Compose Email:");
+
+                final AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                        .setTitle( Html.fromHtml("<font color='#37474F'>Compose Email to:</font>"))
+                        .setView(R.layout.custom_dialog)
+                        .create();
+
+                dialog.show();
+                //dialog.getWindow().setLayout(1000, 500);
+
+                final EditText composeTo = (EditText)dialog.findViewById(R.id.editText);
+
+
+                Log.v("logger",composeTo.getText().toString());
+
+                FloatingActionButton innerFab =  (FloatingActionButton)dialog.findViewById(R.id.innerfab);
+                innerFab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+//                        dialog.cancel();
+//
+//                        Toast toast = Toast.makeText(context, "Mail is Sending", Toast.LENGTH_SHORT);
+//                        toast.show();
+
+
+
+                        new SendMailTask(MainActivity.this).execute(composeTo.getText().toString());
+
+
+                        //toast.setText("Mail Sent");
+
+                        dialog.cancel();
+
+                    }
+                });
+
+                dialog.show();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(!MainActivity.hasActiveInternetConnection(getApplicationContext()))
+        {
+
+            Toast.makeText(MainActivity.this,"Oops! Check Network Connection",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public static boolean hasActiveInternetConnection(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null && netInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    public class SendMailTask extends AsyncTask<String,String,Integer> {
+
+        private ProgressDialog statusDialog;
+        private Activity sendMailActivity;
+        Toast toast=new Toast(MainActivity.this);
+
+        private String LOG_CAT = SendMailTask.class.getName();
+
+        public SendMailTask(Activity activity) {
+            sendMailActivity = activity;
+
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            int result = 0;
+            try {
+                Log.i(LOG_CAT , "About to instantiate GMail...");
+
+                publishProgress("Processing");
+                GMail androidEmail = new GMail(params[0].toString());
+
+                //publishProgress("Preparing mail message....");
+                androidEmail.createEmailMessage();
+
+                //publishProgress("Sending email....");
+                result=androidEmail.sendEmail();
+
+                //publishProgress("Email Sent.");
+            } catch (Exception e) {
+                Log.e("SendMailTask", e.getMessage(), e);
+            }
+            return result;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            toast.makeText(MainActivity.this,values[0],Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            toast.cancel();
+            if(integer.intValue()==0)
+            {
+                Toast.makeText(MainActivity.this, "Invalid Mail Address", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this,"Mail Sent",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+}
